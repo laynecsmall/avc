9fd25e4a5bddb57c923bf781fd71c017fa28dc0c
Layne Small <laynecsmall@gmail.com>
2013-09-19 15:05:40 +1200

    initial commit

---
+++ test/test.ino
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+// we need fundamental FILE definitions and printf declarations
+#include <stdio.h>
+
+// create a FILE structure to reference our UART output function
+
+static FILE uartout = {0} ;
+
+// create a output function
+// This works because Serial.write, although of
+// type virtual, already exists.
+static int uart_putchar (char c, FILE *stream)
+{
+    Serial.write(c) ;
+    return 0 ;
+}
+
+void p(char *fmt, ... ){
+        char tmp[128]; // resulting string limited to 128 chars
+        va_list args;
+        va_start (args, fmt );
+        vsnprintf(tmp, 128, fmt, args);
+        va_end (args);
+        Serial.print(tmp);
+}
+
+void setup(){
+  // Start the UART
+   Serial.begin(9600) ;
+
+   // fill in the UART file descriptor with pointer to writer.
+   fdev_setup_stream (&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+   // The uart is the standard output device STDOUT.
+   stdout = &uartout ;
+	
+   String move[] = {"foo","bar","bazinga","dobby"};
+   Serial.println(sizeof(move));
+   for (int i=0;i <sizeof(move)-1;i++){
+	   Serial.println(move[i]);
+   }
+}
+void loop(){
+}
